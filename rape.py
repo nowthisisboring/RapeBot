a630afd9316554a5065b9bcee49d6660b422b33a
nowthisisboring <68215293+nowthisisboring@users.noreply.github.com>
2021-12-10 19:35:48 +0000

    Create rape.py

---
+++ rape.py
@@ -0,0 +1,13 @@
+#Python Rape Bot
+
+print("Hey! I'm Rape Bot")
+
+method = input("How would you like to be raped\n")
+
+print("Are you sure you want " + method)
+
+answer = input("Yes/No\n")
+
+if answer=='yes': print("Proceeds to " + method)
+
+if answer=='no': print("Retarded pussy")
